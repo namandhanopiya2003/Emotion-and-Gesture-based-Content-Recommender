3b05010933acabdccf1270c578fad0fcef4b2d3b
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-11-13 16:52:31 +0530

    Updated documentation

--- Emotion_Gesture_Content_Recommender/utils/preprocess_emotion_data.py
+++ Emotion_Gesture_Content_Recommender/utils/preprocess_emotion_data.py
@@ -2,12 +2,17 @@ import pandas as pd
 import numpy as np
 
 def preprocess_fer_csv(input_path='data/emotion/fer2013.csv'):
+    
     df = pd.read_csv(input_path)
     df.dropna(inplace=True)
     df['emotion'] = df['emotion'].astype(int)
+
+    # Converts space-separated pixel strings to 48x48 arrays, normalized to [0, 1]
     df['pixels'] = df['pixels'].apply(lambda x: np.fromstring(x, sep=' ').reshape(48, 48).astype('float32') / 255.0)
+    
     return df
 
 if __name__ == "__main__":
     df = preprocess_fer_csv()
     print("Sample record:\n", df.head(1))
+
