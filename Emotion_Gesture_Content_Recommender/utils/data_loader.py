07fefb29a69e1d2307f543eb21d4f79be0dbb832
Naman Dhanopiya <iamnamandhanopiya@gmail.com>
2025-11-13 16:51:43 +0530

    Improved code readability

--- Emotion_Gesture_Content_Recommender/utils/data_loader.py
+++ Emotion_Gesture_Content_Recommender/utils/data_loader.py
@@ -1,7 +1,13 @@
 import pandas as pd
 
 def load_mock_user_data(path='data/mock_user_data.csv'):
+    
+    # Loads CSV into a DataFrame
     df = pd.read_csv(path)
+
+    # Extracts features and labels
     features = df[['likes', 'comments', 'gesture_score', 'emotion_id']]
     labels = df['content_type']
+    
     return features, labels
+
